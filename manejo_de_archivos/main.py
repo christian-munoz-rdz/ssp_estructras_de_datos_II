ba14ecfe5690fdf84b6376fcc307e990c6b0905f
Christian Muñoz <chgeovany.15@gmail.com>
2022-09-17 01:29:36 -0500

    Corregido

--- manejo_de_archivos/main.py
+++ manejo_de_archivos/main.py
@@ -1,4 +1,4 @@
-def main():
+def write_new_file():
 
     try:
         archivo = open('archivo.txt','r')
@@ -13,6 +13,15 @@ def main():
     finally:
         print('Fin del programa')
         archivo.close()
+
+def main():
+    try:
+        archivo = open('archivo.txt','r')
+        print(archivo.read())
+    except IOError:
+        print('No se pudo abrir el archivo')
+    finally:
+        print('Fin del programa')
 
 if __name__ == '__main__':
     main()
