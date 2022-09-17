0c5e22a315b15e97a142666bbe2d5401efc284be
Christian Muñoz <chgeovany.15@gmail.com>
2022-09-17 01:03:14 -0500

    Manejo de archivos

---
+++ manejo_de_archivos/main.py
@@ -0,0 +1,18 @@
+def main():
+
+    try:
+        archivo = open('archivo.txt','r')
+        texto = archivo.readlines()
+        nuevo_texto = []
+        for i in range(len(texto)):
+            nuevo_texto += [texto[i]] + ['*\n']
+        nuevo_archivo=open("archivo2.txt",mode="a+",encoding="utf-8")
+        nuevo_archivo.writelines(nuevo_texto)
+    except IOError:
+        print('No se pudo abrir el archivo')
+    finally:
+        print('Fin del programa')
+        archivo.close()
+
+if __name__ == '__main__':
+    main()
