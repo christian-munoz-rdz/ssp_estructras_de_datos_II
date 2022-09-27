b9339766c25eba8cdd26fe14155c95ad056aba80
Christian Muñoz <chgeovany.15@gmail.com>
2022-09-27 13:44:30 -0500

    Descifrado cesar

---
+++ escritura_archivos/main.py
@@ -0,0 +1,50 @@
+def encode(nombreArchivo):
+    letters = "bcdefghijklmnopqrstuvwxyza"
+    try:
+        archivo = open(nombreArchivo,'r')
+        caracter = archivo.read(1)
+        nuevo_texto = ""
+        while caracter:
+            if caracter != caracter.upper():
+                try:
+                    nuevo_texto += letters[letters.index(caracter)+1]
+                except IndexError:
+                    nuevo_texto += 'b'
+            else:
+                nuevo_texto += caracter
+            caracter = archivo.read(1)
+    except IOError:
+        print('No se pudo abrir el archivo')
+    finally:
+        archivo.close()
+    return nuevo_texto
+
+def decode(archivo1,archivo2,texto):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    try:
+        codificado = open(archivo1,mode="a+",encoding="utf-8")
+        codificado.write(texto)
+        decodificado = open(archivo2,mode="a+",encoding="utf-8")
+        texto_decodificado = ""
+        for i in range(len(texto)):
+            caracter = texto[i]
+            if caracter != caracter.upper():
+                try:
+                    texto_decodificado += letters[letters.index(caracter)-1]
+                except IndexError:
+                    nuevo_texto += 'z'
+            else:
+                texto_decodificado += caracter
+        decodificado.write(texto_decodificado)
+    except IOError:
+        print('No se pudo abrir el archivo')
+    finally:
+        codificado.close()
+        decodificado.close()
+
+def main():
+    texto_encriptado = encode("original.txt")
+    decode("codificado.txt","decodificado.txt",texto_encriptado)
+
+if __name__ == "__main__":
+    main()
